07cb632cfc9bf77b92cbbd0f264eec29c634ff28
francesta <huntleyfrancesta@gmail.com>
2021-03-31 13:08:49 -0400

    commit tree b8b443c201c0d5d954ab54d87b78d98b6e2b74e0

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solution in this file!
+const a = 42;
+
+function distanceFromHqInBlocks(b) {
+
+    if (a > b) {
+
+        let x = a - b;
+        return x;
+    }
+
+    let x = b - a;
+    return x;
+
+}
+
+function distanceFromHqInFeet(b) {
+
+    let feet = distanceFromHqInBlocks(b) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(b, c) {
+    let blocksToDropOff;
+
+
+    if (b >= c) {
+
+        blocksToDropOff = (b - c);
+
+    } else {
+
+        blocksToDropOff = (c - b);
+
+    }
+
+
+    let totalFeet = blocksToDropOff * 264;
+    return totalFeet;
+
+}
+
+
+function calculatesFarePrice(b, c) {
+    let totalFeet = distanceTravelledInFeet(b, c);
+
+    if (totalFeet <= 400) {
+        return 0;
+    }
+    if (totalFeet > 400 && totalFeet <= 2000) {
+
+        return (totalFeet - 400) * 0.02;
+
+    }
+    if (totalFeet > 2000 && totalFeet <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+
+
+}
